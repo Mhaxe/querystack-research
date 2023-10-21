24a1db69dcfccf5f2fb5c313f7d8ab8a5a689b24
akyensamuel <138381921+akyensamuel@users.noreply.github.com>
2023-10-21 11:29:59 +0000

    Tried the categories sorting code

---
+++ wodoame/dummy_data.js
@@ -0,0 +1,254 @@
+//Define a list of 50 questions with dictionaries
+const questions = [
+    {
+        "title": "How to learn Python?",
+        "content": "I'm new to programming and want to learn Python. What's the best way to get started?",
+        "tags": ["Python", "Programming", "Beginner"]
+    },
+    {
+        "title": "What is machine learning?",
+        "content": "Can someone explain the concept of machine learning and its applications?",
+        "tags": ["Machine Learning", "AI", "Data Science"]
+    },
+    {
+        "title": "Web development frameworks",
+        "content": "Which web development frameworks are popular for building modern websites?",
+        "tags": ["Web Development", "Framework", "Frontend", "Backend"]
+    },
+    {
+        "title": "How to write clean code?",
+        "content": "I'm looking for tips on writing clean and maintainable code. Any advice?",
+        "tags": ["Coding", "Best Practices", "Software Development"]
+    },
+    {
+        "title": "Getting started with data analysis",
+        "content": "I want to start a career in data analysis. Where should I begin?",
+        "tags": ["Data Analysis", "Career", "Data Science"]
+    },
+    {
+        "title": "JavaScript vs. Python for web development",
+        "content": "Which is better for web development, JavaScript or Python?",
+        "tags": ["JavaScript", "Python", "Web Development"]
+    },
+    {
+        "title": "Artificial intelligence in healthcare",
+        "content": "How is AI being used in healthcare, and what are its benefits?",
+        "tags": ["Artificial Intelligence", "Healthcare", "Medicine"]
+    },
+    {
+        "title": "Cybersecurity best practices",
+        "content": "What are the best practices for securing a computer network against cyberattacks?",
+        "tags": ["Cybersecurity", "Network Security", "IT"]
+    },
+    {
+        "title": "Introduction to blockchain technology",
+        "content": "Can someone explain the basics of blockchain technology and its applications?",
+        "tags": ["Blockchain", "Cryptocurrency", "Technology"]
+    },
+    {
+        "title": "Becoming a data scientist",
+        "content": "How can I become a data scientist and what skills are required?",
+        "tags": ["Data Science", "Career", "Skills"]
+    },
+    {
+        "title": "Mobile app development platforms",
+        "content": "Which platforms and languages are commonly used for mobile app development?",
+        "tags": ["Mobile App Development", "iOS", "Android", "Programming"]
+    },
+    {
+        "title": "Cloud computing services",
+        "content": "What are the major cloud computing providers, and what services do they offer?",
+        "tags": ["Cloud Computing", "AWS", "Azure", "Google Cloud"]
+    },
+    {
+        "title": "Natural language processing (NLP)",
+        "content": "How does natural language processing work, and what are its applications?",
+        "tags": ["NLP", "Artificial Intelligence", "Machine Learning"]
+    },
+    {
+        "title": "Frontend vs. backend development",
+        "content": "What's the difference between frontend and backend development, and which should I choose?",
+        "tags": ["Web Development", "Frontend", "Backend", "Programming"]
+    },
+    {
+        "title": "Data privacy and GDPR",
+        "content": "What is GDPR, and how does it impact data privacy and businesses?",
+        "tags": ["GDPR", "Data Privacy", "Regulation"]
+    },
+    {
+        "title": "Introduction to data visualization",
+        "content": "How can I create effective data visualizations to communicate data insights?",
+        "tags": ["Data Visualization", "Analytics", "Data Analysis"]
+    },
+    {
+        "title": "Python libraries for data science",
+        "content": "What are the essential Python libraries for data analysis and data science?",
+        "tags": ["Python", "Data Science", "Libraries"]
+    },
+    {
+        "title": "Game development with Unity",
+        "content": "How can I get started with game development using the Unity game engine?",
+        "tags": ["Game Development", "Unity", "Programming"]
+    },
+    {
+        "title": "IoT (Internet of Things) applications",
+        "content": "What are some real-world applications of IoT technology?",
+        "tags": ["IoT", "Technology", "Applications"]
+    },
+    {
+        "title": "Agile software development methodology",
+        "content": "Explain the Agile software development methodology and its principles.",
+        "tags": ["Agile", "Software Development", "Methodology"]
+    },
+    {
+        "title": "Big data and Hadoop",
+        "content": "What is big data, and how does Hadoop help in processing and analyzing it?",
+        "tags": ["Big Data", "Hadoop", "Data Processing"]
+    },
+    {
+        "title": "Machine learning algorithms",
+        "content": "Which machine learning algorithms are commonly used for different tasks?",
+        "tags": ["Machine Learning", "Algorithms", "Data Science"]
+    },
+    {
+        "title": "Introduction to cybersecurity",
+        "content": "What is cybersecurity, and why is it important in today's digital world?",
+        "tags": ["Cybersecurity", "Security", "Technology"]
+    },
+    {
+        "title": "Databases and SQL",
+        "content": "How does SQL work, and what are the basics of working with databases?",
+        "tags": ["Databases", "SQL", "Data Management"]
+    },
+    {
+        "title": "Software testing best practices",
+        "content": "What are the best practices for testing software and ensuring quality?",
+        "tags": ["Software Testing", "Quality Assurance", "QA"]
+    },
+    {
+        "title": "Getting started with Raspberry Pi",
+        "content": "How can I start using Raspberry Pi for DIY projects and learning electronics?",
+        "tags": ["Raspberry Pi", "DIY", "Electronics"]
+    },
+    {
+        "title": "Ethical hacking and penetration testing",
+        "content": "What is ethical hacking, and how can I become a penetration tester?",
+        "tags": ["Ethical Hacking", "Penetration Testing", "Cybersecurity"]
+    },
+    {
+        "title": "DevOps practices and tools",
+        "content": "Explain the DevOps culture, practices, and common tools used in DevOps pipelines.",
+        "tags": ["DevOps", "Development", "Operations"]
+    },
+    {
+        "title": "Artificial neural networks",
+        "content": "How do artificial neural networks function, and what are their applications in AI?",
+        "tags": ["Artificial Neural Networks", "AI", "Deep Learning"]
+    },
+    {
+        "title": "Responsive web design",
+        "content": "What is responsive web design, and how can I make websites that work well on all devices?",
+        "tags": ["Web Design", "Responsive Design", "UI/UX"]
+    },
+    {
+        "title": "Blockchain and cryptocurrency",
+        "content": "What is the relationship between blockchain technology and cryptocurrencies like Bitcoin?",
+        "tags": ["Blockchain", "Cryptocurrency", "Bitcoin"]
+    },
+    {
+        "title": "Computer vision and image processing",
+        "content": "How does computer vision work, and what are its applications in image processing?",
+        "tags": ["Computer Vision", "Image Processing", "Machine Learning"]
+    },
+    {
+        "title": "Mobile app monetization strategies",
+        "content": "What are the different strategies for monetizing a mobile app or game?",
+        "tags": ["Mobile App", "Monetization", "Business"]
+    },
+    {
+        "title": "Data engineering and ETL processes",
+        "content": "Explain the role of data engineers and the ETL (Extract, Transform, Load) process in data pipelines.",
+        "tags": ["Data Engineering", "ETL", "Data Processing"]
+    },
+    {
+        "title": "JavaScript frameworks and libraries",
+        "content": "Which JavaScript frameworks and libraries are popular for web development?",
+        "tags": ["JavaScript", "Frontend", "Frameworks", "Libraries"]
+    },
+    {
+        "title": "AI in the automotive industry",
+        "content": "How is artificial intelligence used in the automotive industry, especially in self-driving cars?",
+        "tags": ["AI", "Automotive", "Self-Driving Cars"]
+    },
+    {
+        "title": "Software version control with Git",
+        "content": "How does Git work, and why is it essential for software version control?",
+        "tags": ["Git", "Version Control", "Software Development"]
+    },
+    {
+        "title": "Data privacy in social media",
+        "content": "What are the privacy risks associated with using social media, and how can users protect their data?",
+        "tags": ["Social Media", "Privacy", "Data Protection"]
+    },
+    {
+        "title": "Cloud-native architecture and microservices",
+        "content": "Explain the concepts of cloud-native architecture and microservices in modern application development.",
+        "tags": ["Cloud-Native", "Microservices", "Architecture"]
+    },
+    {
+        "title": "3D printing technology",
+        "content": "How does 3D printing work, and what are its applications in various industries?",
+        "tags": ["3D Printing", "Technology", "Manufacturing"]
+    },
+    {
+        "title": "Frontend development with React",
+        "content": "How can I get started with frontend web development using React?",
+        "tags": ["React", "Frontend Development", "JavaScript"]
+    },
+    {
+        "title": "Internet safety for kids and parents",
+        "content": "What are some internet safety tips for children, and how can parents ensure their kids' online safety?",
+        "tags": ["Internet Safety", "Parenting", "Children"]
+    },
+    {
+        "title": "Data analysis with Python and Pandas",
+        "content": "How can I perform data analysis using Python and the Pandas library?",
+        "tags": ["Python", "Data Analysis", "Pandas"]
+    },
+    {
+        "title": "Quantum computing fundamentals",
+        "content": "Explain the basics of quantum computing and its potential impact on computing technologies.",
+        "tags": ["Quantum Computing", "Technology", "Computing"]
+    },
+    {
+        "title": "Ethical considerations in AI and robotics",
+        "content": "What ethical challenges arise in the field of artificial intelligence and robotics, and how can they be addressed?",
+        "tags": ["Ethics", "AI", "Robotics"]
+    },
+    {
+        "title": "Machine learning in finance",
+        "content": "How is machine learning applied in the financial industry for tasks like risk assessment and fraud detection?",
+        "tags": ["Machine Learning", "Finance", "Risk Assessment"]
+    },
+    {
+        "title": "Augmented reality (AR) and virtual reality (VR)",
+        "content": "What are AR and VR technologies, and what are their applications in entertainment and business?",
+        "tags": ["Augmented Reality", "Virtual Reality", "Technology"]
+    },
+    {
+        "title": "Web scraping with Python",
+        "content": "How can I scrape data from websites using Python and libraries like Beautiful Soup and Scrapy?",
+        "tags": ["Web Scraping", "Python", "Data Extraction"]
+    },
+    {
+        "title": "Understanding the Internet of Things (IoT)",
+        "content": "What is the Internet of Things, and how is it transforming various industries?",
+        "tags": ["IoT", "Technology", "Smart Devices"]
+    },
+    {
+        "title": "Remote work best practices",
+        "content": "What are the best practices for remote work, and how can individuals and teams stay productive?",
+        "tags": ["Remote Work", "Productivity", "Work from Home"]
+    },
+]
+
